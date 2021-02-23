d19e8e5f8f6cd6f9c24adbd443cd10813c470ca5
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-02-24 00:09:56 +0530

    first commit

---
+++ tewt.py
@@ -0,0 +1,9 @@
+def Si(): return input()
+def Ii(): return int(input())
+def Li(): return list(map(int, input().split()))
+def Mi(): return map(int, input().split())
+
+
+for _ in range(Ii()):
+    n = Ii()
+    wenfjk
