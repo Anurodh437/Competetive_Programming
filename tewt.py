6e4cadfbe6fad4c7454f31c8ea030cad0d6ef397
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-02-24 00:03:37 +0530

    added new file

--- tewt.py
+++ tewt.py
@@ -3,17 +3,9 @@ def Ii(): return int(input())
 def Li(): return list(map(int, input().split()))
 def Mi(): return map(int, input().split())
 
-def primecheck(a):
-    if(n==2):
-        return True
-        exit()
-    if(n%2==0):
-        return False
-        exit()
-    for i in range(3,int(n**(1/2))+1,2):
-        if(n%i==0):
-            return False
-            break
-    else:
-        return False
-       
+
+lst = [1,2,3,4,5,6]
+for j in enumerate(lst):
+    print(j)
+for i in range(0,len(lst),2):
+    print(lst)
