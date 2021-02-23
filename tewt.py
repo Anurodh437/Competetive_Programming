64a1a1f38b7457ddcd84ed34b12ecb1262312291
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-02-24 00:15:27 +0530

    added some changes

--- tewt.py
+++
@@ -1,9 +0,0 @@
-def Si(): return input()
-def Ii(): return int(input())
-def Li(): return list(map(int, input().split()))
-def Mi(): return map(int, input().split())
-
-
-for _ in range(Ii()):
-    n = Ii()
-    wenfjk
