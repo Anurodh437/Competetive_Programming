b4712f59dbfd91780f92cfe81ae9b471243c1b8f
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-04-05 22:41:40 +0530

    new solutios added

---
+++ Strong Language.py
@@ -0,0 +1,26 @@
+def Si(): return input()
+def Ii(): return int(input())
+def Li(): return list(map(int, input().split()))
+def Mi(): return map(int, input().split())
+
+
+for _ in range(Ii()):
+    res = 0
+    count = 0
+
+    n,k = Mi()
+    st = Si()
+    if '*'not in st:
+        print("NO")
+    else:
+        for i in range(n):
+            if st[i] == '*':
+                res += 1
+                if res == k:
+                    print("YES")
+                    count += 1
+                    break
+            else:
+                res = 0
+        if count==0:
+            print("NO")    
--- pawricode.py
+++ pawricode.py
@@ -3,11 +3,7 @@ def Ii(): return int(input())
 def Li(): return list(map(int, input().split()))
 def Mi(): return map(int, input().split())
 
+
 for _ in range(Ii()):
-    w1,w2,x1,x2,M = Mi()
-    a = x1*M
-    b = x2*M
-    if a <= abs(w1-w2) <= b:
-        print(1)
-    else:
-        print(0)
+    st = Si()
+    print(st.replace("party", "pawri"))
